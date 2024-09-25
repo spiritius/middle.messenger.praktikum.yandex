d339062a0a6ceabda2cc7b03601092fe185629ff
Anna Khlystova <spiritiuster@gmail.com>
2024-09-25 19:52:46 +0300

    feat: test for block.ts done

--- src/core/block.spec.ts
+++ src/core/block.spec.ts
@@ -16,7 +16,9 @@ describe('Block', () => {
       }
       render(): string {
         return `
-          <div id="content-body">{{content}}</div>
+          <div>
+            <div id="content-body">{{ content }}</div>
+          </div>
           `;
       }
     }
@@ -32,12 +34,46 @@ describe('Block', () => {
     const content = 'sprint 4';
     const testComponent = new TestBlock({ content });
     
-    const getContent = testComponent.element?.querySelector('#content-body')?.innerHTML;
-
-    console.log(testComponent.element?.innerHTML); // sprint 4
-    console.log(getContent); // undefined
+    const getContent = testComponent.element?.querySelector('#content-body')?.textContent;
 
     expect(getContent).to.be.eq(content);
+  });
+
+  it('should have reactive behavior', () => {
+    const content = 'lorem ipsum';
+    const testComponent = new TestBlock({ content: 'sprint 4' });
+    
+    testComponent.setProps({ content });
+    const getContent = testComponent.element?.querySelector('#content-body')?.textContent;
+
+    expect(getContent).to.be.eq(content);
+  });
+
+  it('should put the event and handle it', () => {
+    const handlerStub = sinon.stub();
+    const testComponent = new TestBlock({ events: {
+      click: handlerStub
+    } });
+    
+    const event = new MouseEvent('click');
+    testComponent.element?.dispatchEvent(event);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    expect(handlerStub.calledOnce).to.be.true;
+  });
+
+  it('should call dispatchComponentDidMount() method', () => {
+    const clock = sinon.useFakeTimers();
+    const testComponent = new TestBlock();
+
+    const spyCDM = sinon.spy(testComponent, 'componentDidMount');
+
+    const element = testComponent.getContent();
+    document.body.append(element!);
+    clock.next();
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    expect(spyCDM.calledOnce).to.be.true;
   });
 
 });
